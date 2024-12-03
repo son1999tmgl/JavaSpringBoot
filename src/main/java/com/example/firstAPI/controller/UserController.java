edc7bcb940d9baeaa04c2bba8ec782333cba2b90
son1999tmgl <120165137+son1999tmgl@users.noreply.github.com>
2024-12-03 11:09:50 +0700

    first commit

---
+++ src/main/java/com/example/firstAPI/controller/UserController.java
@@ -0,0 +1,22 @@
+package com.example.firstAPI.controller;
+
+
+import com.example.firstAPI.dto.request.UserRequestDTO;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+    @PostMapping("/")
+    public String createUser(@RequestBody UserRequestDTO user){
+        return "created";
+    }
+    @GetMapping("/{id}")
+    public String getUser(@PathVariable("id") String id){
+        return "id: " +  id;
+    }
+    @PutMapping("/")
+    public String updateUser(@RequestBody UserRequestDTO user){
+        return user.getEmail();
+    }
+}
