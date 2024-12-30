e376f750aff85cb1d991ce6a7bb91209e6f7ffdc
son1999tmgl <120165137+son1999tmgl@users.noreply.github.com>
2024-12-30 16:57:21 +0700

    exception

---
+++ src/main/java/com/example/firstAPI/exception/GlobalExceptionHandler.java
@@ -0,0 +1,49 @@
+package com.example.firstAPI.exception;
+
+
+import jakarta.validation.ConstraintViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+
+import java.util.Date;
+
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+//    request: request từ api
+//    @ExceptionHandler({MethodArgumentNotValidException.class, MissingServletRequestParameterException.class, ConstraintViolationException.class})
+//    @ResponseStatus(HttpStatus.BAD_REQUEST)
+
+    public ErrorResponse handlerValidationException(Exception e, WebRequest request){
+        System.out.println("======================>");
+        ErrorResponse errorResponse = new ErrorResponse();
+        errorResponse.setTimestamp(new Date());
+        errorResponse.setStatus(HttpStatus.BAD_REQUEST.value());
+        errorResponse.setPath(request.getDescription(false).replace("uri=", " "));
+        errorResponse.setError(HttpStatus.BAD_REQUEST.getReasonPhrase());
+        String message = e.getMessage();
+        if (e instanceof MethodArgumentNotValidException) {
+            int start = message.lastIndexOf("[") + 1;
+            int end = message.lastIndexOf("]") - 1;
+            message = message.substring(start, end);
+            errorResponse.setError("Invalid Payload");
+            errorResponse.setMessage(message);
+        } else if (e instanceof MissingServletRequestParameterException) {
+            errorResponse.setError("Invalid Parameter");
+            errorResponse.setMessage(message);
+        } else if (e instanceof ConstraintViolationException) {
+            errorResponse.setError("Invalid Parameter");
+            errorResponse.setMessage(message.substring(message.indexOf(" ") + 1));
+        } else {
+            errorResponse.setError("Invalid Data");
+            errorResponse.setMessage(message);
+        }
+        return errorResponse;
+
+    }
+}
