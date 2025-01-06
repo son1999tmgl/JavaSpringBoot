560cccca8d98557201ef94f4c41c177eeb1c2283
son1999tmgl <120165137+son1999tmgl@users.noreply.github.com>
2025-01-06 11:39:47 +0700

    CORS: WebMvcConfigurer

--- src/main/java/com/example/firstAPI/configuration/AppConfig.java
+++ src/main/java/com/example/firstAPI/configuration/AppConfig.java
@@ -1,14 +1,23 @@
 package com.example.firstAPI.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class AppConfig implements WebMvcConfigurer
+public class AppConfig
 {
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        WebMvcConfigurer.super.addCorsMappings(registry);
+    @Bean
+    public WebMvcConfigurer corsFilter(){
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("*")
+                        .allowedHeaders("*");
+            }
+        };
     }
 }
